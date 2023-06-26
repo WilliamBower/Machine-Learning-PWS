783682ab88713680f304d1c9a5f3299643a9c64d
WilliamBower <william.mat.bower@gmail.com>
2023-06-26 14:48:25 +0200

    0.1.2

---
+++ control_code/ThreadTest.py
@@ -0,0 +1,21 @@
+from threading import Thread
+import time
+
+def Network():
+    for i in range(3):
+        print(1)
+        time.sleep(5)
+        print(2)
+
+def main2():
+    for i in range(5):
+        print(3)
+        time.sleep(3)
+        print(4)
+
+t1 = Thread(target=main1)
+t2 = Thread(target=main2)
+if __name__ == "__main__":
+    t1.start()
+    t2.start()
+    #in plaats van main()
---
+++ execution_code/DataGather.py
@@ -0,0 +1,33 @@
+from picamera import PiCamera
+import time
+import os
+
+folder_location = "" #input folder path
+images = 0 #input image amount
+time_gaps = 0
+
+camera = PiCamera()
+camera.resolution(1920, 1080)
+
+def main():
+    #check for valid save location
+    if os.path.isdir(folder_location) == False:
+        os.mkdir(folder_location)
+        print(f"New folder created at {folder_location}")
+    
+    #start gathering
+    camera.start_preview()
+    for i in range(images):
+        if (i+1) % 10 == 0 and i != 0:
+            print(f"{i+1} images taken")
+        camera.capture(f"{folder_location}/{i}.png")
+        time.sleep(time_gaps)
+    camera.stop_preview()
+
+    if len(os.listdir(folder_location)) == images:
+        print("Data gathering succesful")
+    else:
+        print(f"{images} images expected, {len(os.listdir(folder_location))} images taken")
+
+if __name__ == "__main__":
+    main()
